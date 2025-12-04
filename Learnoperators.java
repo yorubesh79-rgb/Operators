3ced4caac7974beb6414d5e34164e295b6ea60d1
yorubesh79-rgb <yorubesh79@gmail.com>
2025-12-04 15:16:24 +0530

    Add files via upload
    operators

---
+++ Learnoperators.java
@@ -0,0 +1,19 @@
+package learnautomation1;
+
+public class Learnoperators {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=20;
+		System.out.println("a+b:"+ (a+b));
+		System.out.println("a-b:"+ (a-b));
+		System.out.println("a*b:"+ (a+b));
+		System.out.println("a/b:"+ (a/b));
+		System.out.println("a%b:"+ (a%b));
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
